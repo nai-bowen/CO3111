b40aaf49b96e6566704a52cebd6f0df51101b6ea
nai-bowen <nainaibow@outlook.com>
2025-05-03 14:24:39 +0100

    7 - EVSem

--- IMP-Interpreter/EvSem.hs
+++ IMP-Interpreter/EvSem.hs
@@ -1,9 +1,3 @@
-
---------------------------------------------------------------------- 
--- SML EVALUATION SEMANTICS FOR IMP                                   
--- Roy Crole and Paula Severi 2025                                            
---------------------------------------------------------------------- 
-
 module EvSem
 
 where
@@ -32,59 +26,55 @@ fromBop GrEq = (>=)
 --  Code the function evalint for evaluating Integer Expressions
 ----------------------------------------------------------------
 
-
-{-- 
 evalint :: IntExp -> State -> (Either Error Int) 
-
 evalint (Int m) s = Right m 
 
-evalint  (Var v) s =  ???
+evalint (Var v) s = lookUp s v
 
-evalint  (IopExp (op,e1,e2))  s = do 
-             m1 <-  evalint e1 s 
-             m2 <-  evalint e2 s 
-             Right (fromIop op m1 m2)
-                                        
-                                                      
-                                     
---}
-
+evalint (IopExp (op,e1,e2)) s = do 
+    m1 <- evalint e1 s 
+    m2 <- evalint e2 s 
+    Right (fromIop op m1 m2)
                                       
  ---------------------------------------------------------------------
 -- Code the function evalbool for evaluating Boolean Expressions
 -----------------------------------------------------------------------
 
-{--
 evalbool :: BoolExp -> State -> (Either Error Bool)
-evalbool  (Bool b) s = Right b 
-evalbool  (BopExp (op,e1,e2))  s =  ???
-                                                       
---}
+evalbool (Bool b) s = Right b 
+evalbool (BopExp (op,e1,e2)) s = do
+    m1 <- evalint e1 s
+    m2 <- evalint e2 s
+    Right (fromBop op m1 m2)
                                          
 --------------------------------------------------------------------------
 -- Code the function evalcom for evaluating Commands
 ---------------------------------------------------------------------------
 
-{-- 
 evalcom :: Com -> State -> (Either Error State) 
+evalcom (Ass(v,e)) s = do
+    val <- evalint e s
+    Right (update s v val)
 
-
-evalcom (Ass(v,e)) s = ???
-
-evalcom (Seq(co1,co2))  s1  =  ???
+evalcom (Seq(co1,co2)) s1 = do
+    s2 <- evalcom co1 s1
+    evalcom co2 s2
                                       
-        
-evalcom (If(be,co1,co2)) s = ???
+evalcom (If(be,co1,co2)) s = do
+    b <- evalbool be s
+    if b then evalcom co1 s else evalcom co2 s
                
-
-evalcom (While(be,co)) s1 = ???
+evalcom (While(be,co)) s1 = do
+    b <- evalbool be s1
+    if b 
+    then do
+        s2 <- evalcom co s1
+        evalcom (While(be,co)) s2
+    else Right s1
           
-          
-          
-evalcom (Repeat(co, be)) s1 = ???
-
---}
-                                 
-         
-       
-       
+evalcom (Repeat(co, be)) s1 = do
+    s2 <- evalcom co s1
+    b <- evalbool be s2
+    if b
+    then Right s2
+    else evalcom (Repeat(co, be)) s2
