f3b11bb668fd185fcf96691931e3dd15301ad52f
nai-bowen <nainaibow@outlook.com>
2025-05-03 12:58:12 +0100

    3- First implementation of basic and AST

--- AST.hs
+++ AST.hs
@@ -1,4 +1,3 @@
-  
 --------------------------------------------------------------------
 -- HASKELL ABSTRACT SYNTAX TREES FOR IMP                  
 -- Roy Crole and Paula Severi 2025                                           
@@ -11,45 +10,41 @@ type B = Bool
 type Z = Int
 type FileName = String
 
-
-data Error =  SyntaxError | UninitializedVar
+data Error = SyntaxError | UninitializedVar
               deriving (Eq, Show)   
               
-
--- COMPLETE THE TYPE FOR THE STATE 
---type State = ???
-
+-- State represents memory as list of variable-value pairs
+type State = [(V, Z)]
 
 data Instruction = Run FileName | Eval Prog | Quit 
                    deriving (Eq,Show)
 
-type Prog  = (Code,State)  
+type Prog = (Code,State)  
 
-
-data Code  = E IntExp | C Com
+data Code = E IntExp | C Com
              deriving (Eq,Show)
 
-
-data Bop =   Le | Gr | LeEq | GrEq
+data Bop = Le | Gr | LeEq | GrEq
              deriving (Eq,Show)
 
 data Iop = Plus | Minus | Times 
              deriving (Eq,Show)
 
--- COMPLETE THE FOLLOWING DATA TYPES
-
---data Com   =  ???
+-- Commands in IMP
+data Com = Ass (V, IntExp)                      -- Assignment: x := e
+         | Seq (Com, Com)                       -- Sequence: c1 ; c2
+         | If (BoolExp, Com, Com)               -- Conditional: if b then c1 else c2
+         | While (BoolExp, Com)                 -- While loop: while b do c
+         | Repeat (Com, BoolExp)                -- Repeat-until: repeat c until b
+         deriving (Eq, Show)
              
---data BoolExp   =  ???
+-- Boolean expressions
+data BoolExp = Bool B                           -- Boolean constant: true, false
+             | BopExp (Bop, IntExp, IntExp)     -- Comparison: e1 < e2, e1 <= e2, etc.
+             deriving (Eq, Show)
              
---data IntExp   =  ???       
-             
-
-
-
-
-              
-
-
-
-
+-- Integer expressions
+data IntExp = Int Z                             -- Integer constant: 5, -3, etc.
+            | Var V                             -- Variable: x, y, etc.
+            | IopExp (Iop, IntExp, IntExp)      -- Operation: e1 + e2, e1 - e2, etc.
+            deriving (Eq, Show)
--- Basic.hs
+++ Basic.hs
@@ -1,4 +1,3 @@
- 
 ------------------------------------------------------------------- 
 -- HASKELL BASIC LIST AND STRING PROCESSING FUNCTIONS           
 -- Roy L. Crole and Paula Severi 2025                                      
@@ -9,25 +8,16 @@ module Basic where
 import AST
 
 -- code mem 
-
-{-- 
 mem :: Eq a => a -> [a] -> Bool
-mem x []     = ???
-mem x (h:t)  = ???
---}
+mem x []     = False
+mem x (h:t)  = x == h || mem x t
 
 -- code lookUp
-
-{-- 
 lookUp :: Eq a => [(a,b)] -> a -> Either Error b
-lookUp [] v             = ???
-lookUp ((x,y):pairs) v  = ???
---}
+lookUp [] v             = Left UninitializedVar
+lookUp ((x,y):pairs) v  = if v == x then Right y else lookUp pairs v
 
 -- code update
-
-{--
 update :: Eq a => [(a, b)] -> a -> b -> [(a, b)]
-update [] v z = ???
-update ((v,z):pairs) v' z'  = ???
---}
+update [] v z = [(v, z)]
+update ((v',z'):pairs) v z  = if v == v' then (v, z):pairs else (v',z'):(update pairs v z)
